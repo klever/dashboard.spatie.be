3786716cd0c1d88ce907ee87a2f69fde123ad9ec
Keoghan Litchfield <keoghan@klever.co.uk>
2017-07-28 14:17:42 +0100

    Check we're not overwriting some functions in helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,11 +1,15 @@
 <?php
 
-function markdownToHtml(string $markdown)
-{
-    return (new Parsedown)->text($markdown);
+if (! function_exists('markdownToHtml')) {
+    function markdownToHtml(string $markdown)
+    {
+        return (new Parsedown)->text($markdown);
+    }
 }
 
-function usingNodeServer(): bool
-{
-    return config('broadcasting.default') === 'laravel-echo-server';
+if (! function_exists('usingNodeServer')) {
+    function usingNodeServer(): bool
+    {
+        return config('broadcasting.default') === 'laravel-echo-server';
+    }
 }
